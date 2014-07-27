dc785a010a5a232ac4b48b8212c0ecd707486256
Ben Gamari <bgamari.foss@gmail.com>
2014-07-27 18:40:58 -0400

    adc: Fix sampling
    It was still quite wrong

--- firmware/adc.c
+++ firmware/adc.c
@@ -2,6 +2,7 @@
 #include "util.h"
 #include <stddef.h>
 #include <libopencm3/stm32/adc.h>
+#include <libopencm3/cm3/nvic.h>
 
 static uint16_t *buffer = NULL;
 static unsigned int remaining = 0;
@@ -9,8 +10,14 @@ static unsigned int remaining = 0;
 void adc_take_sample(unsigned int length, uint8_t *sequence, uint16_t *buf)
 {
   buffer = buf;
-  remaining = 0;
+  remaining = length;
   adc_set_regular_sequence(ADC1, length, sequence);
+  adc_disable_scan_mode(ADC1);
+  adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_16CYC);
+  adc_set_single_conversion_mode(ADC1);
+  adc_enable_eoc_interrupt(ADC1);
+  ADC_CR2(ADC1) |= ADC_CR2_EOCS;
+  nvic_enable_irq(NVIC_ADC1_IRQ);
 
   // turn on ADC
   while (ADC_SR(ADC1) & ADC_SR_ADONS);
@@ -24,12 +31,14 @@ void adc_take_sample(unsigned int length, uint8_t *sequence, uint16_t *buf)
   WAIT_FOR(remaining == 0);
 
   // turn off ADC
+  nvic_disable_irq(NVIC_ADC1_IRQ);
   while (!(ADC_SR(ADC1) & ADC_SR_ADONS));
   adc_off(ADC1);
 }
 
 void adc1_isr(void)
 {
+  // also clears IRQ flag
   *buffer = adc_read_regular(ADC1);
   buffer++;
   remaining--;
