4c8ef9577b0e184c73dd9050069001ad61cc1dca
Ben Gamari <bgamari.foss@gmail.com>
2014-07-27 20:31:05 -0400

    usart: Non-blocking tx

--- firmware/usart.c
+++ firmware/usart.c
@@ -1,3 +1,4 @@
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/rcc.h>
@@ -6,18 +7,42 @@
 on_line_recv_cb on_line_recv;
 
 char rx_buf[255];
-unsigned int rx_head;
+unsigned int rx_head = 0;
+
+char tx_buf[255];
+unsigned int tx_head = 0; // write in position
+unsigned int tx_tail = 0; // read out position
+
+static unsigned int waiting_tx_bytes(void)
+{
+  if (tx_head < tx_tail)
+    return tx_head + sizeof(tx_buf) - tx_tail;
+  else
+    return tx_head - tx_tail;
+}
+
+int usart_putc(char c)
+{
+  unsigned int next = (tx_head + 1) % sizeof(tx_buf);
+  if (waiting_tx_bytes() == sizeof(tx_buf))
+    return 0;
+
+  tx_buf[tx_head] = c;
+  tx_head = next;
+  usart_enable_tx_interrupt(USART1);
+  return 1;
+}
 
 void usart_write(const char* c, unsigned int length)
 {
   for (unsigned int i=0; i<length; i++)
-    usart_send_blocking(USART1, c[i]);
+    usart_putc(c[i]);
 }
 
 void usart_print(const char* c)
 {
   for (const char* i = c; *i != 0; i++)
-    usart_send_blocking(USART1, *i);
+    usart_putc(*i);
 }
 
 unsigned int usart_readline(char* buffer, unsigned int length)
@@ -46,6 +71,7 @@ void usart_init(void)
   usart_set_mode(USART1, USART_MODE_TX_RX);
   usart_set_baudrate(USART1, 9600);
   usart_enable_rx_interrupt(USART1);
+  nvic_enable_irq(NVIC_USART1_IRQ);
 }
 
 void usart1_isr(void)
@@ -59,6 +85,15 @@ void usart1_isr(void)
     } else {
       rx_buf[rx_head] = c;
       rx_head++;
+    }
+  }
+
+  if (usart_get_flag(USART1, USART_SR_TXE)) {
+    if (waiting_tx_bytes() > 0) {
+      usart_send(USART1, tx_buf[tx_tail]);
+      tx_tail++;
+    } else {
+      usart_disable_tx_interrupt(USART1);
     }
   }
 }
