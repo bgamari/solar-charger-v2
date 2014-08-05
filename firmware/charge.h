abed315590ce24ed57b0c4a7791b7c28e22e49a5
Ben Gamari <bgamari.foss@gmail.com>
2014-08-05 09:25:36 -0400

    charge: Export sample cache

--- firmware/charge.h
+++ firmware/charge.h
@@ -5,6 +5,12 @@ enum charge_rate {
   CHARGE,  // maximum power-point tracking
 };
 
+// Cached sensor values (read-only)
+extern uint32_t bat_i; // battery charge current (uA)
+extern uint32_t bat_v; // battery voltage (mV)
+extern uint32_t bat_temp; // battery temperature (centi-Kelvin)
+extern uint32_t input_v; // charger input voltage (mV)
+
 void charge_init(void);
 
 void charge_start(enum charge_rate rate);
