849afc338330a25d4204c139e292690980adfa56
Ben Gamari <bgamari.foss@gmail.com>
2014-08-23 17:21:41 -0400

    Fix plot script

--- firmware/plot.py
+++ firmware/plot.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as pl
 import numpy as np
+import numpy.ma as ma
 import sys
 
 signal_labels = {
@@ -12,32 +13,29 @@ signal_labels = {
     'power': 'power (mW)',
 }
 
-signals = set()
-data = []
-cur = {}
+data = {}
+idx = 0
 for l in open(sys.argv[1]):
     if l.startswith('iterate'):
-        data.append(cur)
-        cur =  {}
+        idx += 1
         continue
 
     k,_,v = l.partition('=')
     if v is '':
         continue
     v = v.split()[0]
-    signals.add(k)
     try:
-        cur[k] = float(v)
+        v = float(v)
+        data.setdefault(k, []).append((idx, v))
     except:
         #print l
         pass
 
-have = set(signal_labels.keys()).intersection(signals)
-for i,k in enumerate(have):
+for i,k in enumerate(data):
     data[k] = np.array(data[k])
-    label = signals[k]
-    pl.subplot(len(have), 1, i)
-    pl.plot(data[k][:,0], data[k][:,1], '-o', label=label)
+    label = signal_labels.get(k, k)
+    pl.subplot(len(data), 1, i)
+    pl.plot(data[k][:,0], data[k][:,1], '+', label=label)
     pl.ylabel(label)
 
 pl.show()
